bde218cc1f453ed6792d0d4d8ceb09514e1aca5f
sharad.shivmath <sharad.shivmath@zapcg.com>
2023-01-27 10:48:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const init = () => {
+    setInterval(() => {
+        
+    }, interval);
+}
+// ref: https://www.booksfact.com/puranas/muhurtas-described-ramayana.html#:~:text=(1)%20Swati%2C%20(2,after%20Abhijit%20constitutes%2020%20muhurtas.
+const muhurtams = {
+    "6": "रुद्र",
+    "6.48": "आहि",
+    ""
+}
