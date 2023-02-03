1c0e77b7ef2e1fa5fc83345fdf5a206f1499383b
sharad.shivmath <sharad.shivmath@zapcg.com>
2023-02-03 11:26:09 +0530

    fixed muhurta calculation

--- index.js
+++ index.js
@@ -7,18 +7,25 @@ const init = () => {
 const updateClock = () => {
     let dayStartMuhurtam = moment('06 00', 'HH mm');    
     let currDate = new Date();
-    
-    let currMoment = moment(`${currDate.getHours()} ${currDate.getMinutes()} ${currDate.getSeconds()}`, 'HH mm ss');
-    let hourDiff = currMoment.diff(dayStartMuhurtam, 'hours');
-    if(hourDiff < 0) {
-        let midNightMoment = moment('00 00 00', 'HH mm ss');
-        hourDiff = 18 + currMoment.diff(midNightMoment, 'hours');
+    if(currDate.getHours() >= 0 && currDate.getHours() < 6) {
+        currDate.setFullYear(1992, 11, 7);
     }
-    let currMuhurtamIndex = Math.floor((hourDiff*60)/48);
-    let currMuhurtam = muhurtams3[currMuhurtamIndex + 1];
-
+    else {
+        currDate.setFullYear(1992, 11, 6);
+    }
+    let currMoment = moment(`${currDate.getHours()} ${currDate.getMinutes()} ${currDate.getSeconds()}`, 'HH mm ss');
+    
+    let currMuhurtam = '';
+    let currMuhurtamIndex = 1;
+    for(let key in muhurtams4) {
+        if(currDate >= muhurtams4[key][0] && currDate < muhurtams4[key][1]) {
+            currMuhurtam = key;
+            break;
+        }
+        currMuhurtamIndex++;
+    }
     let currMuhurtamStartMoment = moment(`${muhurtams[currMuhurtam][0].hour} ${muhurtams[currMuhurtam][0].min} 00`, 'HH mm ss');
-    let currKaalam = Math.floor(currMuhurtamStartMoment.diff(currMoment, 'minutes')/1.6);
+    let currKaalam = Math.floor(currMoment.diff(currMuhurtamStartMoment, 'minutes')/1.6);
 
     let currKaalamStartMoment = moment(`${muhurtams[currMuhurtam][0].hour} ${muhurtams[currMuhurtam][0].min + Math.floor(currKaalam * 1.6)}`, 'HH mm');
     let secondsPassed = currMoment.diff(currKaalamStartMoment, 'seconds');
@@ -33,11 +40,11 @@ const updateClock = () => {
     const kashtamDiv = document.getElementById('kashtam-digit');
     kashtamDiv.innerHTML = kashtam;
 
-    
+
     // debug display
     document.getElementById('western-time').innerHTML = currMoment._i;
     document.getElementById('muhurta-start-western').innerHTML = currMuhurtamStartMoment._i;
-    document.getElementById('minutes-elapsed-western').innerHTML = currMuhurtamStartMoment.diff(currMoment, 'minutes');
+    document.getElementById('minutes-elapsed-western').innerHTML = currMoment.diff(currMuhurtamStartMoment, 'minutes');
 };
 
 init();
@@ -147,3 +154,36 @@ const muhurtams =
     "द्युमद्गद्युति",
     "ब्रह्म",
     "समुद्रम"]
+
+    const muhurtams4 = {
+        "रुद्र": [new Date(1992, 11, 6, 6, 0, 0), new Date(1992, 11, 6, 6, 48)],
+        "आहि": [new Date(1992, 11, 6, 6, 0, 0), new Date(1992, 11, 6, 6, 48)],
+        "मित्र": [new Date(1992, 11, 6, 7, 36, 0), new Date(1992, 11, 6, 8, 24)],
+        "पितॄ": [new Date(1992, 11, 6, 8, 24), new Date(1992, 11, 6, 9, 12)],
+        "वसु": [new Date(1992, 11, 6, 9, 12), new Date(1992, 11, 6, 10, 0)],
+        "वाराह": [new Date(1992, 11, 6, 10, 0), new Date(1992, 11, 6, 10, 48)],
+        "विश्वेदेवा": [new Date(1992, 11, 6, 10, 48), new Date(1992, 11, 6, 11, 36)],
+        "विधि": [new Date(1992, 11, 6, 11, 36), new Date(1992, 11, 6, 12, 24)],
+        "सतमुखी": [ new Date(1992, 11, 6, 12, 24), new Date(1992, 11, 6, 13, 12)],
+        "पुरुहूत": [new Date(1992, 11, 6, 13, 12), new Date(1992, 11, 6, 14, 0)],
+        "वाहिनी": [ new Date(1992, 11, 6, 14, 0), new Date(1992, 11, 6, 14, 48)],
+        "नक्तनकरा": [ new Date(1992, 11, 6, 14, 48), new Date(1992, 11, 6, 15, 36)],
+        "वरुण": [ new Date(1992, 11, 6, 15, 36), new Date(1992, 11, 6, 16, 24)],
+        "अर्यमन्": [ new Date(1992, 11, 6, 16, 24), new Date(1992, 11, 6, 17, 12)],
+        "भग": [ new Date(1992, 11, 6, 17, 12), new Date(1992, 11, 6, 18, 0)],
+        "गिरीश": [ new Date(1992, 11, 6, 18, 0), new Date(1992, 11, 6, 18, 48)],
+        "अजपाद": [ new Date(1992, 11, 6, 18, 48), new Date(1992, 11, 6, 19, 36)],
+        "अहिर्बुध्न्य": [ new Date(1992, 11, 6, 19, 36), new Date(1992, 11, 6, 20, 24)],
+        "पुष्य": [ new Date(1992, 11, 6, 20, 24), new Date(1992, 11, 6, 21, 12)],
+        "अश्विनी": [ new Date(1992, 11, 6, 21, 12), new Date(1992, 11, 6, 22, 0)],
+        "यम": [ new Date(1992, 11, 6, 22, 0), new Date(1992, 11, 6, 22, 48)],
+        "अग्नि": [ new Date(1992, 11, 6, 22, 48), new Date(1992, 11, 6, 23, 36)],
+        "विधातृ": [ new Date(1992, 11, 6, 23, 36), new Date(1992, 11, 7, 0, 24)],
+        "क्ण्ड": [ new Date(1992, 11, 7, 0, 24), new Date(1992, 11, 7, 1, 12)],
+        "अदिति": [ new Date(1992, 11, 7, 1, 12), new Date(1992, 11, 7, 2, 0)],
+        "जीव/अमृत": [ new Date(1992, 11, 7, 2, 0), new Date(1992, 11, 7, 2, 48)],
+        "विष्णु": [ new Date(1992, 11, 7, 2, 48), new Date(1992, 11, 7, 3, 36)],
+        "द्युमद्गद्युति": [ new Date(1992, 11, 7, 3, 36), new Date(1992, 11, 7, 4, 24)],
+        "ब्रह्म": [ new Date(1992, 11, 7, 4, 24), new Date(1992, 11, 7, 5, 12)],
+        "समुद्रम": [ new Date(1992, 11, 7, 5, 12), new Date(1992, 11, 7, 6, 0)]
+    }
